1a983ba96e4ff67f05862c953d294d6487874933
Michael Samper <msamper@ufl.edu>
2017-09-02 18:52:19 -0400

    working version w/ CSS

--- listingController.js
+++ listingController.js
@@ -1,14 +1,28 @@
-angular.module('listings').controller('ListingsController', ['$scope', 'Listings', 
+angular.module('listings').controller('ListingsController', ['$scope', 'Listings',
   function($scope, Listings) {
     $scope.listings = Listings;
     $scope.detailedInfo = undefined;
 
-    /* 
-      Implement these functions in the controller to make your application function 
-      as described in the assignment spec. 
+    /*
+      Implement these functions in the controller to make your application function
+      as described in the assignment spec.
      */
-    $scope.addListing = function() {};
-    $scope.deleteListing = function(index) {};
-    $scope.showDetails = function(index) {};
+    $scope.addListing = function() {
+      $scope.listings.push({"code":$scope.code,"name":$scope.name,
+      "coordinates":{"latitude":$scope.latitude,
+      "longitude":$scope.longitude},"address":$scope.address});
+      $scope.code="";
+      $scope.name="";
+      $scope.latitude="";
+      $scope.longitude="";
+      $scope.address="";
+    };
+    $scope.deleteListing = function(index) {
+      $scope.listings.splice(index, 1);
+    };
+    $scope.showDetails = function(index) {
+      // TO DO
+      $scope.detailedInfo = $scope.listings[index];
+    };
   }
 ]);
