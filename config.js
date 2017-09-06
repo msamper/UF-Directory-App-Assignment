5553118782b615075d38655a85858a124be45f58
Michael Samper <msamper@ufl.edu>
2017-09-06 12:56:21 -0400

    Added more styling

---
+++ config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  db: {
+    uri: 'mongodb://michael:test@ds121014.mlab.com:21014/bootcamp-3-ms', //place the URI of your mongo database here.
+  }
+};
+
+/* Now go to the JSONtoMongo.js file and include this file as a variable named 'config' with a require() */
