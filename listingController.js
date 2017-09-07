14d8afe0f49fca086b2118f8db02fc8c536eac31
Michael Samper <msamper@ufl.edu>
2017-09-06 21:43:23 -0400

    Fixed bug

--- listingController.js
+++ listingController.js
@@ -17,11 +17,12 @@ angular.module('listings').controller('ListingsController', ['$scope', 'Listings
       $scope.longitude="";
       $scope.address="";
     };
-    $scope.deleteListing = function(index) {
+    $scope.deleteListing = function(listing) {
+      var index = $scope.listings.indexOf(listing);
       $scope.listings.splice(index, 1);
     };
-    $scope.showDetails = function(index) {
-      // TO DO
+    $scope.showDetails = function(listing) {
+      var index = $scope.listings.indexOf(listing);
       $scope.detailedInfo = $scope.listings[index];
     };
   }
